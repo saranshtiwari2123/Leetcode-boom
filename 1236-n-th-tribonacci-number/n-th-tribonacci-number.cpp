ff37c2dd149b68197e9e2b2e9e223b63d0df5f1c
Saransh Tiwari <saranshtiwari9984@gmail.com>
2026-03-08 14:46:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (7.30%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
-    int trio(int n, vector<int>& dp){
-        if(n==0 || n==1) return n;
-        if(n==2) return 1;
-        if(dp[n]!=-1) return dp[n];
-        dp[n] = trio(n-1,dp) + trio(n-2,dp) + trio(n-3,dp);
-        return dp[n];
-    }
     int tribonacci(int n) {
-        vector<int> dp(n+1,-1);
-        return trio(n, dp);
+        if(n==1 || n==0) return n;
+        if(n==2) return 1; 
+        vector<int> dp(n+1);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        for(int i=3;i<=n;i++){
+            dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
+        }
+        return dp[n];
     }
 };
