8f184aee81b89c40c414470fea549a1ca3a8073a
Saransh Tiwari <saranshtiwari9984@gmail.com>
2026-03-08 14:57:40 +0530

    Time: 1 ms (4.59%) | Memory: 8 MB (47.70%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -3,13 +3,17 @@ public:
     int tribonacci(int n) {
         if(n==1 || n==0) return n;
         if(n==2) return 1; 
-        vector<int> dp(n+1);
+        vector<int> dp(4);
         dp[0] = 0;
         dp[1] = 1;
         dp[2] = 1;
-        for(int i=3;i<=n;i++){
-            dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
+        for(int i=0;i<n-2;i++){
+            dp[3] = dp[2]+dp[1]+dp[0];
+            dp[0] = dp[1];
+            dp[1] = dp[2];
+            dp[2] = dp[3];
+
         }
-        return dp[n];
+        return dp[3];
     }
 };
