6e70999fc1c4d4f8b37d81ec93acb80acc4434ae
Saransh Tiwari <saranshtiwari9984@gmail.com>
2026-03-09 23:22:38 +0530

    Time: 389 ms (31.24%) | Memory: 0B (100.00%) - LeetSync

--- 182-duplicate-emails/duplicate-emails.sql
+++ 182-duplicate-emails/duplicate-emails.sql
@@ -1,4 +1,5 @@
-select Email 
-from Person
-group by email
-having count(email)>1;
+select distinct p1.email as Email 
+from Person as p1
+join person as p2
+on p1.email = p2.email
+AND p1.id<>p2.id;
