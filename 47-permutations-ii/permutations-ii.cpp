151cf30303e236a92ef3ea8ab7ad66f83d4904df
Saransh Tiwari <saranshtiwari9984@gmail.com>
2026-08-03 16:08:45 +0530

    Time: 3 ms (63.99%) | Memory: 11.8 MB (68.94%) - LeetSync

---
+++ 47-permutations-ii/permutations-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    void solve(vector<int>& nums, vector<int>& v, vector<bool>& flag){
+        if(v.size()==nums.size()){
+            ans.push_back(v);
+            return;
+        }
+        for(int i=0;i<nums.size();i++){
+            if(i>0 && nums[i]==nums[i-1] && flag[i-1]) continue;
+            if(!flag[i]){
+                v.push_back(nums[i]);
+                flag[i] = true;
+
+                solve(nums, v, flag);
+
+                flag[i] = false;
+                v.pop_back();
+            }
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<int> v;
+        vector<bool> flag(nums.size(), false);
+        solve(nums, v, flag);
+        return ans;
+    }
+};
