86218d5352fe09c2c71fd27d07c400a17e8f4bb2
Saransh Tiwari <saranshtiwari9984@gmail.com>
2026-08-07 16:44:30 +0530

    Time: 1 ms (62.32%) | Memory: 27.4 MB (90.51%) - LeetSync

---
+++ 695-max-area-of-island/max-area-of-island.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int countArea(vector<vector<int>>& grid, int i, int j, int& count){
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size()) return 0;
+        if(grid[i][j]==0) return 0;
+        grid[i][j] = 0;
+        count++;
+        int l = countArea(grid, i+1, j, count);
+        int r = countArea(grid, i, j+1, count);
+        int up = countArea(grid, i-1, j, count);
+        int down = countArea(grid, i, j-1, count);
+        return count;
+    }
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int area = 0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                int count = 0;
+                area = max(area, countArea(grid, i, j, count));
+            }
+        }
+        return area;
+    }
+};
